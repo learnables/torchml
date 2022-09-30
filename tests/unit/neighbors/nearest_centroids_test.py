4f25ef66a90d207dbf94fb92ce9d805de24dff2b
Andy Chen <anindy@icloud.com>
2022-09-30 13:57:43 -0700

    add documentations and tests

--- tests/unit/neighbors/nearest_centroids_test.py
+++ tests/unit/neighbors/nearest_centroids_test.py
@@ -4,31 +4,27 @@ import torch
 import torchml as ml
 import sklearn.neighbors as neighbors
 
+# define numbers of classes & features
 CLS = 10
-FEA = 3
+FEA = 10
 
 
 class Testcentroids(unittest.TestCase):
 
     def test_kneighbors(self):
         
-
-        for i in range(10):
-            #X = np.random.randn(CLS, FEA)
-            X = np.random.randint(1,5,size=(CLS, FEA))
-            y = np.random.randint(1,5,size=CLS)
+        for i in range(100):
+            X = np.random.randint(1,100,size=(CLS, FEA))
+            y = np.random.randint(1,100,size=CLS)
             torchX = torch.from_numpy(X)
             torchy = torch.from_numpy(y)
             ref = neighbors.NearestCentroid()
             cent = ml.neighbors.NearestCentroid()
             ref.fit(X,y)
             cent.fit(torchX,torchy)
-            #samp = np.random.randn(CLS,FEA)
-            samp = np.random.randint(1,5,size=(CLS, FEA))
+            samp = np.random.randint(1,100,size=(CLS, FEA))
             refres=ref.predict(samp)
             centres = cent.predict(torch.from_numpy(samp)).numpy()
-            print("X: {}; \ny: {}; \nsamp: {}".format(X,y,samp))
-            print("Trial{}: ref: {} v.s. cent: {}".format(i,refres,centres))
             self.assertTrue(np.array_equal(refres,centres))
 
 if __name__ == '__main__':
