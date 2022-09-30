f41c02db625286a6de13d7d7a9c91abe33cd2fa6
Andy Chen <anindy@icloud.com>
2022-09-30 13:57:43 -0700

    add documentations and tests

--- torchml/neighbors/nearest_centroid.py
+++ torchml/neighbors/nearest_centroid.py
@@ -1,76 +1,62 @@
 import torch
 
-#import torchml as ml
+import torchml as ml
 
-import math
-
-from scipy import sparse as sp
-
-#from ..utils import LabelEncoder
-
-#import numpy as np
-
-#class NearestCentroid(ml.Model):
-class NearestCentroid():
+class NearestCentroid(ml.Model):
 
 
     """
     ## Description
 
-    Implementation of scikit-learn's Nearest centroid APIs using pytorch.
+    Implementation of scikit-learn's Nearest centroid APIs using pytorch. 
+    Euclidean metric by default. 
 
     ## References
     1. Scikit-learn library (https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.NearestCentroid.html)
-
-    ## Arguments
-    * metric : str or callable, default="euclidean"
-        Metric to use for distance computation. Default is "minkowski", which
-        results in the standard Euclidean distance when p = 2. 
-        See the documentation of `scipy.spatial.distance
-        <https://docs.scipy.org/doc/scipy/reference/spatial.distance.html>`_ 
-        Note that "wminkowski", "seuclidean" and "mahalanobis" are not
-        supported.
-
-    * shrink_threshold : float, default=None
-        Threshold for shrinking centroids to remove features.
+    2. Nearest Centroid: A Bridge between Statistics and Machine Learning https://ieeexplore.ieee.org/abstract/document/9368396
 
 
+    ## Example
+
+    ~~~python
+    import numpy as np
+    import torchml as ml
+    X = np.array([[3,2],[1,8],[3,5],[9,7],[7,7]])
+    y = np.array([9,1,9,8,3])
+    samp = np.array([[6,1],[8,9],[5,3],[8,2],[9,8]])
+    torchX = torch.from_numpy(X)
+    torchy = torch.from_numpy(y)
+    centroid = ml.neighbors.NearestCentroid()
+    centroid.fit(torchX,torchy)
+    output = centroid.predict(torch.from_numpy(samp)).numpy()
+    ~~~
     """
 
 
-    def __init__(self, metric="euclidean", *, shrink_threshold=None):
+    def __init__(self, metric="euclidean"):
         self.metric = metric
-        self.shrink_threshold = shrink_threshold
 
     def fit(self, X: torch.Tensor, y: torch.Tensor):
         """
+        ## Description 
         Fit the NearestCentroid model according to the given training data.
-        Parameters
-        ----------
-        * X : {array-like, sparse matrix} of shape (n_samples, n_features)
+
+        ## Arguments
+        * 'X' (torch.Tensor): array-like, sparse matrix of shape (n_samples, n_features)
             Training vector, where `n_samples` is the number of samples and
-            `n_features` is the number of features.
-            Note that centroid shrinking cannot be used with sparse matrices.
-        * y : array-like of shape (n_samples,)
-            Target values.
-        Returns
-        -------
-        self : object
-            Fitted estimator.
+            `n_features` is the number of features
+        * 'y' (torch.Tensor): array-like of shape (n_samples,) Target values
         """
+        
+        #Convert to float64 tensors
         X.to(torch.float64)
         y.to(torch.float64)
-
-        if self.metric == "precomputed":
-            raise ValueError("Precomputed is not supported.")
 
         n_samples, n_features = X.shape
 
         # y_ind: idx, y_classes: unique tensor
         self.y_type = y[0].dtype
-        y_classes, y_ind = torch.unique(y,sorted=True,return_inverse=True)
-        #print ("y_ind: {} and y_classes: {}".format(y_ind,y_classes))
-        #Class: dict[y in Y] = X
+        y_classes, y_ind = torch.unique(y,sorted=False,return_inverse=True)
         self.classes_ = classes = y_classes
         n_classes = classes.size(dim=0)
         if n_classes < 2:
@@ -82,48 +68,39 @@ class NearestCentroid():
         # Mask mapping each class to its members.
         self.centroids_ = torch.empty((n_classes, n_features), dtype=torch.float64)
         # Number of clusters in each class.
-        # TODO: nk used for shrink
-        nk = torch.zeros(n_classes)
 
         for cur_class in range(n_classes):
             center_mask = y_ind == cur_class
-            #nk[cur_class] = torch.sum(center_mask)  
-            #print("cur_class: {}".format(cur_class))
-            #print("centermask: {}".format(center_mask))
-            #print("x:{}".format(X))
 
-            # XXX: Update other averaging methods according to the metrics.
             if self.metric != "euclidean":
                 raise ValueError("Only Euclidian is supported.")
 
             else:
 
-                #print("haha")
-                #print("EUCLIDEAN:")
-                #print("centroids b4 add: {}".format(self.centroids_))
-                #print("X[center_mask]: {} of type {}".format(X[center_mask],X[center_mask].dtype))
-
-                self.centroids_[cur_class] = torch.nanmean(X[center_mask].to(torch.float64))
-                #print("After add, centroid[{}] becomes {}".format(cur_class,self.centroids_[cur_class]))
-                #self.centroids_[cur_class] = torch.mean(X[0:n_samples-1,center_mask], axis=0)
-                #print(X[0:n_samples-1,center_mask].mean(axis=0))
+                self.centroids_[cur_class] = torch.nanmean(X[center_mask].to(torch.float64),dim=0)
 
         return self
         
     def predict(self, X: torch.tensor)->torch.tensor:
-        #("PREDICTING")
-        #print(X)
-        #print(self.centroids_)
-        #TODO: check if fitted
-        #check_is_fitted(self)
-        
+        """
+        ## Description
+
+        Computes the classes of the sample data
+
+        ## Arguments
+
+        * `X` (torch.Tensor): the sample data, each with n-features 
+
+        ## Return
+
+        * (torch.Tensor): the predicted classes
+
+        """
         if X is None or X.size(dim=0) < 1:
             print("Warning: check input size")
 
-        #print("X size:",X.size())
         ret = torch.empty(X.size(dim=0))
-        # TODO: DATA validate
-        # X = self._validate_data(X, accept_sparse="csr", reset=False)
+
         for i in range(X.size(dim=0)):
             ret[i]= self.classes_[torch.argmin(torch.nn.PairwiseDistance(p=2)(X[i],self.centroids_))] 
         
