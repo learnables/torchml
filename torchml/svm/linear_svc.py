7638bed593e5d75777a4136508f2c50d3c9aa156
David Zhang <210057zzh@gmail.com>
2022-10-30 18:52:42 -0700

    add docs for linearSVC

--- torchml/svm/linear_svc.py
+++ torchml/svm/linear_svc.py
@@ -5,6 +5,77 @@ import cvxpy as cp
 
 
 class LinearSVC(ml.Model):
+    """
+    ## Description
+
+    Unsupervised learner for implementing KNN Classifier.
+
+    ## References
+
+    1. Bernhard E. Boser, Isabelle M. Guyon, and Vladimir N. Vapnik. 1992. A training algorithm for optimal margin classifiers. In Proceedings of the fifth annual workshop on Computational learning theory (COLT '92). Association for Computing Machinery, New York, NY, USA, 144–152. https://doi.org/10.1145/130385.130401
+    2. MIT 6.034 Artificial Intelligence, Fall 2010, [16. Learning: Support Vector Machines](https://youtu.be/_PwhiWxHK8o)
+    3. The scikit-learn [documentation page](https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html) for LinearSVC.
+
+    ## Arguments
+
+    * `penalty` (str {'l1', 'l2'}, default=’l2’):
+        Specifies the norm used in the penalization.
+
+    * `loss` (str {‘hinge’, ‘squared_hinge’}, default=’squared_hinge’):
+        Specifies the loss function. ‘hinge’ is the standard SVM loss.
+
+    * `dual` (bool, default=True):
+        Dummy variable to keep consistency with SKlearn's API, always 'False' for now.
+
+    * `tol` (float, default=1e-4)
+        Tolerance for stopping criteria.
+
+    * `C` (float, default=1.0):
+        Regularization parameter. The strength of the regularization is inversely proportional to C. Must be strictly positive.
+
+    * `multi_class` (str {‘ovr’, ‘crammer_singer’}, default=’ovr’):
+        Dummy variable, always 'ovr' (one class over all the other as a single class)
+
+    * `fit_intercept` (bool, default=True):
+        Whether to calculate the intercept for this model.
+
+    * `intercept_scaling` (float, default=1):
+        Dummy variable to mimic the sklearn API, always 1 for now
+
+    * `class_weight` (dict or str ‘balanced’, default=None):
+        Dummy variable to mimic the sklearn API, always None for now
+
+    * `verbose` (int, default=0):
+        Dummy variable to mimic the sklearn API, always 0 for now
+
+    * `random_state` (int, RandomState instance or None, default=None):
+        Dummy variable to mimic the sklearn API, always None for now
+
+    * `max_iter` (int, default=1000):
+        The maximum number of iterations to be run for the underneath convex solver.
+
+
+    ## Example
+
+    ~~~python
+    import numpy as np
+    from torchml.svm import LinearSVC
+    from sklearn.datasets import make_classification
+
+    x, y = make_classification(
+            n_samples=n_samples,
+            n_features=n_features,
+            n_classes=n_classes,
+            n_informative=n_informative,
+            n_redundant=n_features - n_informative,
+        )
+    svc = LinearSVC(max_iter=1000)
+    svc.fit(torch.from_numpy(x), torch.from_numpy(y))
+    svc.decision_function(torch.from_numpy(x)
+    svc.predict(torch.from_numpy(x))
+    ~~~
+    """
+
     def __init__(
         self,
         penalty="l2",
@@ -39,6 +110,16 @@ class LinearSVC(ml.Model):
         self.loss = loss
 
     def fit(self, X: torch.Tensor, y: torch.Tensor, sample_weight=None):
+        """
+        ## Description
+
+        Initialize the class with training sets
+
+        ## Arguments
+        * `X` (torch.Tensor): the training set
+        * `y` (torch.Tensor, default=None): the class labels for each sample
+
+        """
         if self.C < 0:
             raise ValueError("Penalty term must be positive; got (C=%r)" % self.C)
         self.classes_ = torch.unique(y)
@@ -55,21 +136,26 @@ class LinearSVC(ml.Model):
                 self._fit_with_one_class(X, y, x, sample_weight=sample_weight)
 
     def decision_function(self, X: torch.Tensor) -> torch.Tensor:
+        """
+        ## Description
+
+        Predict confidence scores for samples.
+
+        ## Arguments
+        * `X` (torch.Tensor): the data set for which we want to get the confidence scores.
+
+        """
         return X @ self.coef_.T + self.intercept_
 
     def predict(self, X: torch.Tensor) -> torch.Tensor:
         """
-        Predict class labels for samples in X.
+        ## Description
 
-        Parameters
-        ----------
-        X : {array-like, sparse matrix} of shape (n_samples, n_features)
-            The data matrix for which we want to get the predictions.
+        Predict the class labels for the provided data.
 
-        Returns
-        -------
-        y_pred : ndarray of shape (n_samples,)
-            Vector containing the class labels for each sample.
+        ## Arguments
+
+        * `X` (torch.Tensor): the target point
         """
         scores = self.decision_function(X)
         if len(scores.shape) == 1:
