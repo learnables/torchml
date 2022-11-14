38b44000022f26028e84f6f2273605262d8a6234
David Zhang <210057zzh@gmail.com>
2022-11-13 20:58:54 -0800

    add gpu for neighbors

--- tests/unit/neighbors/nearest_centroids_test.py
+++ tests/unit/neighbors/nearest_centroids_test.py
@@ -13,23 +13,24 @@ CLS = 5
 
 class Testcentroids(unittest.TestCase):
     def test_kneighbors(self):
-
-        for i in range(100):
-            X = np.random.randn(SAMPLES, FEA)
-            y = np.random.randint(1, CLS, size=SAMPLES)
-            torchX = torch.from_numpy(X)
-            torchy = torch.from_numpy(y)
-            ref = neighbors.NearestCentroid()
-            cent = ml.neighbors.NearestCentroid()
-            ref.fit(X, y)
-            cent.fit(torchX, torchy)
-            samp = np.random.randn(SAMPLES, FEA)
-            refres = ref.predict(samp)
-            centres = cent.predict(torch.from_numpy(samp)).numpy()
-            self.assertTrue(np.array_equal(refres, centres))
-            inputSamp = torch.from_numpy(samp)
-            inputSamp.requires_grad = True
-            self.assertTrue(gradcheck(cent.predict, inputSamp, eps=1e-6, atol=1e-3))
+        for i in range(2):
+            device = torch.device('cuda' if torch.cuda.is_available() and i else 'cpu')
+            for i in range(100):
+                X = np.random.randn(SAMPLES, FEA)
+                y = np.random.randint(1, CLS, size=SAMPLES)
+                torchX = torch.from_numpy(X).to(device)
+                torchy = torch.from_numpy(y).to(device)
+                ref = neighbors.NearestCentroid()
+                cent = ml.neighbors.NearestCentroid()
+                ref.fit(X, y)
+                cent.fit(torchX, torchy)
+                samp = np.random.randn(SAMPLES, FEA)
+                refres = ref.predict(samp)
+                centres = cent.predict(torch.from_numpy(samp).to(device)).cpu().numpy()
+                self.assertTrue(np.array_equal(refres, centres))
+                inputSamp = torch.from_numpy(samp).to(device)
+                inputSamp.requires_grad = True
+                self.assertTrue(gradcheck(cent.predict, inputSamp, eps=1e-6, atol=1e-3))
 
 
 if __name__ == "__main__":
