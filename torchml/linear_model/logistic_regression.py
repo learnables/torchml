b86d9c40cee237e77a229f9d2c4243b5999e114f
Lucy Shi <lucys@usc.edu>
2022-11-28 17:12:58 -0800

    implement logistic regression with cvxpylayers

--- torchml/linear_model/logistic_regression.py
+++ torchml/linear_model/logistic_regression.py
@@ -7,16 +7,6 @@ import numpy as np
 import torchml as ml
 
 
-class LogisticRegressionModel(torch.nn.Module):
-    def __init__(self, input_dim: int, output_dim: int):
-        super(LogisticRegressionModel, self).__init__()
-        self.linear = torch.nn.Linear(input_dim, output_dim)
-
-    def forward(self, x: torch.Tensor):
-        outputs = torch.sigmoid(self.linear(x))
-        return outputs
-
-
 class LogisticRegression(ml.Model):
 
     """
