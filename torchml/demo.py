f9a6bd617d20ca96918fa847ab0060a18570b24a
Lucy Shi <lucys@usc.edu>
2022-12-04 14:40:40 -0800

    add a pretrained feature extractor, use the cifar10 image dataset

--- torchml/demo.py
+++ torchml/demo.py
@@ -3,41 +3,66 @@
 import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as dsets
+import torchvision.models as models
+from torch.utils.data import DataLoader
+
 import torchml as ml
 
 
 def main():
-    # Load the MNIST dataset
-    train_dataset = dsets.MNIST(
+    # Set seed
+    torch.manual_seed(42)
+
+    # Load the CIFAR10 dataset
+    train_dataset = dsets.CIFAR10(
         root="./data",
         train=True,
-        transform=transforms.ToTensor(),
+        transform=transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        ),
         download=True,
     )
-    test_dataset = dsets.MNIST(
+    test_dataset = dsets.CIFAR10(
         root="./data",
         train=False,
-        transform=transforms.ToTensor(),
+        transform=transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        ),
     )
+
+    # Create a dataloader
+    train_loader = DataLoader(dataset=train_dataset, batch_size=10000, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=100, shuffle=False)
+
+    # Retrieve the first batch of images and labels
+    X_train = next(iter(train_loader))[0]
+    y_train = next(iter(train_loader))[1]
+    X_test = next(iter(test_loader))[0]
+    y_test = next(iter(test_loader))[1]
+
+    # Initialize a pretrained feature extractor (ResNet18)
+    feature_extractor = models.resnet18(pretrained=True)
+
+    # Extract features from the dataset
+    with torch.no_grad():
+        X_train_features = feature_extractor(X_train)
+        X_test_features = feature_extractor(X_test)
 
     # Create the model
     model = ml.naive_bayes.GaussianNB()
 
-    # Linearize the image data
-    X_train = train_dataset.train_data
-    X_train = X_train.view(X_train.size(0), -1)
-    X_test = test_dataset.test_data
-    X_test = X_test.view(X_test.size(0), -1)
-
     # Train the model
-    model.fit(
-        X_train.float(),
-        train_dataset.train_labels.float(),
-    )
+    model.fit(X_train_features, y_train)
 
     # Test the model
-    preds = model.predict(X_test.float())
-    accuracy = (preds == test_dataset.test_labels).sum().item() / len(preds)
+    preds = model.predict(X_test_features)
+    accuracy = (preds == y_test).sum().item() / len(preds)
     print("Accuracy: {}".format(accuracy))
 
 
